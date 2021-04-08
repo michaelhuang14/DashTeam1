862be29f433c16194fc0b5ef6d09667606fa551f
nathan huang <nathanghuang@gmail.com>
2021-04-08 00:04:39 -0500

    snipers now shoot

--- Assets/Enemy Scripts/SniperCatScript.cs
+++ Assets/Enemy Scripts/SniperCatScript.cs
@@ -6,15 +6,23 @@ public class SniperCatScript : MonoBehaviour
 {
     public GameObject Player;
 
+    public GameObject BulletPF;
+
     private Rigidbody2D RB;
     private SpriteRenderer spriteR;
 
     private float speed;
 
+    bool WithinRange()
+    {
+        Vector3 displacement = transform.position - Player.transform.position;
+        return (displacement.magnitude > 20);
+    }
+
     void Patterns()
     {
         Vector3 displacement = transform.position - Player.transform.position;
-        if (displacement.magnitude < 9)
+        if (displacement.magnitude < 20)
         {
             RB.velocity = Vector3.Normalize(displacement) * speed;
         }
@@ -31,12 +39,28 @@ public class SniperCatScript : MonoBehaviour
         spriteR.material.SetColor("_Color", Color.gray);
         RB = GetComponent<Rigidbody2D>();
 
-        speed = 7f;
+        speed = 3.5f;
+
+        StartCoroutine(MakeBullet());
     }
 
-    private void FixedUpdate()
+    void Update()
     {
         Patterns();
+    }
+
+    IEnumerator MakeBullet()
+    {
+        while (true)
+        {
+            if(WithinRange())
+            {
+                GameObject bullet = Instantiate(BulletPF);
+                bullet.GetComponent<BulletScript>().Player = Player;
+                bullet.transform.position = transform.position;
+            }
+            yield return new WaitForSeconds(0.8f);
+        }
     }
 
     public void Death()
